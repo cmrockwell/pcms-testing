b7900c34e2de385810052aafbb482ad870706fc0
Felix Puetz <felix.puetz@metaproc.de>
2020-11-03 12:44:45 +0100

    add CreatePagePage.js

---
+++ src/pages/CreatePagePage.js
@@ -0,0 +1,33 @@
+const {I} = inject()
+
+class CreatePagePage {
+
+  constructor() {
+    this.url = `/content/admin/pages/pages/create.html/path:/content/pcms_testing/pages`
+  }
+
+  createPage(title, andEdit=false) {
+    I.amOnPage(this.url)
+    I.click('Next')
+    I.fillField('Title', title)
+    I.click('Next')
+
+    if (andEdit) {
+      I.click('Finish and Edit!')
+    } else {
+      I.click('Finish')
+    }
+
+    I.dontSee('create a page')
+    /**
+     * TODO: workaround for broken reactivity in right-panel editor
+     * https://github.com/headwirecom/peregrine-cms/issues/637
+     */
+    if (andEdit) {
+      I.wait(3)
+    }
+  }
+}
+
+module.exports = new CreatePagePage()
+module.exports.CreateNewPagePage = CreatePagePage
