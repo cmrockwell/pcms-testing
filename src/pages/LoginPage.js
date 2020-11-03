6c93e5304a58ef4e777a2b845b3dd7000d8f9683
Felix Puetz <felix.puetz@metaproc.de>
2020-11-03 10:45:36 +0100

    add LoginPage.js

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,31 @@
+const {I} = inject()
+
+class LoginPage {
+
+  constructor() {
+    this.url = '/system/sling/form/login'
+    this.user = {
+      admin: {
+        username: 'admin',
+        password: 'admin'
+      }
+    }
+  }
+
+  loginAs(user) {
+    const {
+      username,
+      password
+    } = this.user[user]
+
+    I.amOnPage(this.url)
+    I.fillField('Username', username)
+    I.fillField('Password', password)
+    I.click('Log In')
+    I.waitForElement('.tenant-tabs', 2 * 60)
+    I.see('your websites', 'h2')
+  }
+}
+
+module.exports = new LoginPage()
+module.exports.LoginPage = LoginPage
