7c029ffb9455a0a9cfcc88e0673b6b2d0bfda72f
Felix Puetz <felix.puetz@metaproc.de>
2020-11-17 08:46:05 +0100

    make use of new "withFormData"

--- src/const.js
+++ src/const.js
@@ -1,3 +1,6 @@
 module.exports = {
-  Tenant: 'pcms_testing'
+  Tenant: 'pcms_testing',
+  ColorPalette: {
+    default: '/content/themecleanflex/pages/css/palettes/default.css'
+  }
 }
--- src/modules/PerApi.js
+++ src/modules/PerApi.js
@@ -1,6 +1,5 @@
-const FormData = require('form-data')
 const {Request} = require('../helpers/ExtendedRest')
-const {Tenant} = require('../const')
+const {Tenant, ColorPalette} = require('../const')
 const {I} = inject()
 
 class PerApi {
@@ -10,63 +9,52 @@ class PerApi {
   }
 
   async createTenant() {
-    const form = new FormData()
-    form.append('fromTenant', 'themecleanflex')
-    form.append('toTenant', Tenant)
-    form.append('tenantTitle', Tenant)
-    form.append('colorPalette',
-        '/content/themecleanflex/pages/css/palettes/default.css')
     return I.sendRestRequest(
         Request.build()
             .withUrl('/admin/createTenant.json')
             .withPOST()
-            .withHeaders(form.getHeaders())
-            .withData(form)
+            .withFormData({
+              fromTenant: 'themecleanflex',
+              toTenant: Tenant,
+              tenantTitle: Tenant,
+              colorPalette: ColorPalette.default
+            })
             .as(`create tenant "${Tenant}"`)
     )
   }
 
   async deleteTenant() {
-    const form = new FormData()
-    form.append('name', 'pcms_testing')
     return I.sendRestRequest(
         Request.build()
             .withUrl(`/admin/deleteTenant.json`)
             .withPOST()
-            .withHeaders(form.getHeaders())
-            .withData(form)
+            .withFormData({name: Tenant})
             .as(`delete tenant "${Tenant}"`)
     )
   }
 
   async createPage(name) {
-    const form = new FormData()
-    form.append('name', name)
-    form.append('templatePath', `/content/${Tenant}/templates/blank`)
-    form.append('title', name)
     return I.sendRestRequest(
         Request.build()
             .withUrl(`/admin/createPage.json/content/${Tenant}/pages`)
             .withPOST()
-            .withHeaders(form.getHeaders())
-            .withData(form)
+            .withFormData({
+              name,
+              templatePath: `/content/${Tenant}/templates/blank`,
+              title: name
+            })
             .as('create page')
     )
   }
 
   async addComponentToPage(page, component, drop, variation) {
     const cmpShortName = component.split('/').reverse()[0]
-    const form = new FormData()
-    form.append('component', component)
-    form.append('drop', drop)
-    form.append('variation', variation)
     return I.sendRestRequest(
         Request.build()
             .withUrl(
                 `/admin/insertNodeAt.json/content/${Tenant}/pages/${page}/jcr:content`)
             .withPOST()
-            .withHeaders(form.getHeaders())
-            .withData(form)
+            .withFormData({component, drop, variation})
             .as(`add component "${cmpShortName}" to page "${page}"`)
     )
   }
